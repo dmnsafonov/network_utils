c7f8908112193b919bbce9794560bfb189ea693e
Dmitry Safonov <dimanss47@dimanss47.net>
2017-11-08 02:11:01 +0000

    design ping data send/receive utilities' interface

--- ping6-datarecv/src/main.rs
+++ ping6-datarecv/src/main.rs
@@ -1,3 +1,26 @@
+#[macro_use] extern crate clap;
+extern crate env_logger;
+#[macro_use] extern crate log;
+
+extern crate linux_network;
+
+use clap::*;
+use linux_network::*;
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .arg(Arg::with_name("bind")
+            .long("bind")
+            .short("b")
+            .takes_value(true)
+            .value_name("INTERFACE")
+            .help("Bind to an interface")
+        ).get_matches();
+
+
 }
--- ping6-datasend/src/main.rs
+++ ping6-datasend/src/main.rs
@@ -1,3 +1,32 @@
+#[macro_use] extern crate clap;
+extern crate env_logger;
+#[macro_use] extern crate log;
+
+extern crate linux_network;
+
+use clap::*;
+use linux_network::*;
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .arg(Arg::with_name("destination")
+            .required(true)
+            .value_name("DESTINATION")
+            .index(1)
+            .help("Messages destination")
+        )
+        .arg(Arg::with_name("messages")
+            .required(true)
+            .value_name("MESSAGES")
+            .multiple(true)
+            .index(2)
+            .help("The messages to send, one argument for a packet")
+        ).get_matches();
+
+
 }
